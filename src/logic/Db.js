b826c81ba6310e7d074662c63ea8372538a8db32
Filip <filip.kudla@snow.dog>
2020-05-05 23:42:19 +0200

    Add logic for database file

---
+++ src/logic/Db.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+import 'firebase/analytics'
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyA6ClIYD2m8sTIBP2ClityYPXdiw7mBCyg",
+  authDomain: "best-before-app-afb09.firebaseapp.com",
+  databaseURL: "https://best-before-app-afb09.firebaseio.com",
+  projectId: "best-before-app-afb09",
+  storageBucket: "best-before-app-afb09.appspot.com",
+  messagingSenderId: "252489393201",
+  appId: "1:252489393201:web:8fe1a44017e555e5b3e110",
+  measurementId: "G-55F6PW84FM"
+})
+
+app.analytics()
+
+export const db = app.database()
+export const auth = app.auth()
+export const currentTime = firebase.database.ServerValue.TIMESTAMP
