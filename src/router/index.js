fbf685cfa5e3b0a9e62f6087b211bdc339705a2f
Filip <filip.kudla@snow.dog>
2020-05-06 00:09:23 +0200

    Build base router and beforeEach hook with checking for auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { auth } from '../logic/Db.js'
+import store from '../store'
+
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
@@ -38,6 +41,24 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  auth.onAuthStateChanged(user => {
+    store.commit('setUser', user)
+
+    if (user && (to.name === 'Login' || to.name === 'Register')) {
+      next({ name: 'Home' })
+    } else if (!user && to.name !== 'Login') {
+      next({ name: 'Login' })
+    } else {
+      next()
+    }
+
+    // else if (!user && to.name !== 'Register') {
+    //   next({ name: 'Register' })
+    // }
+  })
 })
 
 export default router
