0772f3df74cb18b1a56ac1f6994c3a08549ee9dc
Filip <filip.kudla@snow.dog>
2020-05-07 23:43:55 +0200

    Convert to single quotes

--- src/logic/Db.js
+++ src/logic/Db.js
@@ -4,14 +4,14 @@ import 'firebase/auth'
 import 'firebase/analytics'
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyA6ClIYD2m8sTIBP2ClityYPXdiw7mBCyg",
-  authDomain: "best-before-app-afb09.firebaseapp.com",
-  databaseURL: "https://best-before-app-afb09.firebaseio.com",
-  projectId: "best-before-app-afb09",
-  storageBucket: "best-before-app-afb09.appspot.com",
-  messagingSenderId: "252489393201",
-  appId: "1:252489393201:web:8fe1a44017e555e5b3e110",
-  measurementId: "G-55F6PW84FM"
+  apiKey: 'AIzaSyA6ClIYD2m8sTIBP2ClityYPXdiw7mBCyg',
+  authDomain: 'best-before-app-afb09.firebaseapp.com',
+  databaseURL: 'https://best-before-app-afb09.firebaseio.com',
+  projectId: 'best-before-app-afb09',
+  storageBucket: 'best-before-app-afb09.appspot.com',
+  messagingSenderId: '252489393201',
+  appId: '1:252489393201:web:8fe1a44017e555e5b3e110',
+  measurementId: 'G-55F6PW84FM'
 })
 
 app.analytics()
