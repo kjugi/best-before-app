6dc2008a4dc0782f998794f96113f87147250c5e
Filip <filip.kudla@snow.dog>
2020-05-07 23:29:18 +0200

    Update login and register views with message, validation and components

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,51 +1,129 @@
 <template>
   <div class="login">
     <h2 class="login__title">
-      Login form
+      Login to account
     </h2>
 
-    <input
-      type="text"
-      v-model="login"
-    />
+    <form @submit.prevent="triggerLogin">
+      <input-field
+        id="login"
+        label="Login"
+        v-model="login"
+        type="email"
+        :is-error="validation.login.$invalid"
+        :error-message="validation.login.$errors[0] ? validation.login.$errors[0].$message : ''"
+      />
 
-    <input
-      type="password"
-      v-model="password"
-    />
+      <input-field
+        id="password"
+        label="Password"
+        v-model="password"
+        type="password"
+        :is-error="validation.password.$invalid"
+        :error-message="validation.password.$errors[0] ? validation.password.$errors[0].$message : ''"
+      />
 
-    <button @click="loginUser">
-      Login
-    </button>
+      <button
+        :disabled="validation.$invalid"
+        @click="triggerLogin"
+      >
+        Login
+      </button>
+    </form>
 
-    {{ errorMessage }}
+    <portal to="notify-portal">
+      <transition name="fade">
+        <notify
+          v-show="isMessageShowed"
+          :class="messageClasses"
+          :message="message"
+          @click.native="toggleMessage(10)"
+        />
+      </transition>
+    </portal>
   </div>
 </template>
 
 <script>
 import { ref } from '@vue/composition-api'
+import { required, email } from '@vuelidate/validators'
+import useVuelidate from '@vuelidate/core'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
+import InputField from '@/components/InputField.vue'
+import Notify from '@/components/Notify.vue'
+import { initFunction } from '@/logic/Notify.js'
+
 export default {
+  components: {
+    InputField,
+    Notify
+  },
   setup () {
     const login = ref('')
     const password = ref('')
-    let errorMessage = ref('')
 
-    function loginUser() {
+    const validation = useVuelidate(
+      {
+        login: {
+          required,
+          email
+        },
+        password: {
+          required
+        }
+      },
+      { login, password }
+    )
+
+    function triggerLogin() {
       firebase.auth().signInWithEmailAndPassword(login.value, password.value)
-        .catch(error => {
-          // TODO: Error handling
-          errorMessage = error.message
+        .then(() => {
+          showMessage({
+            status: true,
+            messageClass: 'notify--success',
+            message: 'Welcome ' + login.value
+          })
         })
+        .catch(error => {
+          showMessage({
+            status: true,
+            messageClass: 'notify--error',
+            message: error.message
+          })
+        })
+
+      clearFields()
     }
 
+    function clearFields () {
+      login.value = ''
+      password.value = ''
+    }
+
+    // Notify init
+    const {
+      isMessageShowed,
+      message,
+      messageClasses,
+      toggleMessage,
+      showMessage
+    } = initFunction()
+
     return {
+      // Fields
       login,
       password,
-      errorMessage,
-      loginUser
+      // Trigger
+      triggerLogin,
+      // Validation
+      validation,
+      // Notify message
+      isMessageShowed,
+      toggleMessage,
+      messageClasses,
+      message
     }
   }
 }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,47 +1,129 @@
 <template>
   <div class="register">
     <h2 class="register__title">
-      Register form
+      Register to app
     </h2>
 
-    <form>
-      <input type="text" v-model="login"/>
+    <form @submit.prevent="triggerRegister">
+      <input-field
+        id="login"
+        label="Login"
+        v-model="login"
+        type="email"
+        :is-error="validation.login.$invalid"
+        :error-message="validation.login.$errors[0] ? validation.login.$errors[0].$message : ''"
+      />
 
-      <input type="password" v-model="password" />
+      <input-field
+        id="password"
+        label="Password"
+        v-model="password"
+        type="password"
+        :is-error="validation.password.$invalid"
+        :error-message="validation.password.$errors[0] ? validation.password.$errors[0].$message : ''"
+      />
 
-      <button @click="registerUser">
+      <button
+        :disabled="validation.$invalid"
+        @click="triggerRegister"
+      >
         Register
       </button>
-
-      {{ errorMessage }}
     </form>
+
+    <portal to="notify-portal">
+      <transition name="fade">
+        <notify
+          v-show="isMessageShowed"
+          :class="messageClasses"
+          :message="message"
+          @click.native="toggleMessage(10)"
+        />
+      </transition>
+    </portal>
   </div>
 </template>
 
 <script>
 import { ref } from '@vue/composition-api'
+import { required, email } from '@vuelidate/validators'
+import useVuelidate from '@vuelidate/core'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
+import InputField from '@/components/InputField.vue'
+import Notify from '@/components/Notify.vue'
+import { initFunction } from '@/logic/Notify.js'
+
 export default {
+  components: {
+    InputField,
+    Notify
+  },
   setup () {
     const login = ref('')
     const password = ref('')
-    let errorMessage = ref('')
 
-    function registerUser() {
+    const validation = useVuelidate(
+      {
+        login: {
+          required,
+          email
+        },
+        password: {
+          required
+        }
+      },
+      { login, password }
+    )
+
+    function triggerRegister() {
       firebase.auth().createUserWithEmailAndPassword(login.value, password.value)
-        .catch(error => {
-          // TODO: Error handling
-          errorMessage = error.message
+        .then(() => {
+          showMessage({
+            status: true,
+            messageClass: 'notify--success',
+            message: 'Welcome ' + login.value
+          })
         })
+        .catch(error => {
+          showMessage({
+            status: true,
+            messageClass: 'notify--error',
+            message: error.message
+          })
+        })
+
+      clearFields()
     }
 
+    function clearFields () {
+      login.value = ''
+      password.value = ''
+    }
+
+    // Notify init
+    const {
+      isMessageShowed,
+      message,
+      messageClasses,
+      toggleMessage,
+      showMessage
+    } = initFunction()
+
     return {
+      // Fields
       login,
       password,
-      errorMessage,
-      registerUser
+      // Trigger
+      triggerRegister,
+      // Validation
+      validation,
+      // Notify message
+      isMessageShowed,
+      toggleMessage,
+      messageClasses,
+      message
     }
   }
 }
