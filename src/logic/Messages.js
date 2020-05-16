bafaefe692e9e670387e77d7458e122ad24e9b75
Filip <filip.kudla@snow.dog>
2020-05-17 00:18:51 +0200

    WIP logic for sending messages

---
+++ src/logic/Messages.js
@@ -0,0 +1,38 @@
+import { db, messaging } from './Db.js'
+
+// Saves the token to the database if available. If not request permissions
+export function saveToken(userId) {
+  messaging.getToken()
+    .then(currentToken => {
+      if (currentToken) {
+        db.ref(`${userId}/notificationTokens/${currentToken}`).set(true)
+      } else {
+        requestPermission()
+      }
+    })
+    .catch(err => {
+      // TODO: Show this info in notify
+      let notifyMessage = 'An unidentified error has occurred'
+      console.error('Unable to get messaging token.', err)
+
+      if (err.code === 'messaging/permission-default') {
+        notifyMessage = 'You have not enabled notifications on this browser. To enable notifications reload the page and allow notifications using the permission dialog.'
+      } else if (err.code === 'messaging/notifications-blocked') {
+        notifyMessage = 'You have blocked notifications on this browser. To enable notifications follow these instructions: <a href="https://support.google.com/chrome/answer/114662?visit_id=1-636150657126357237-2267048771&rd=1&co=GENIE.Platform%3DAndroid&oco=1">Android Chrome Instructions</a><a href="https://support.google.com/chrome/answer/6148059">Desktop Chrome Instructions</a>'
+      }
+
+      return notifyMessage
+    })
+}
+
+// Requests permission to send notifications on this browser
+export function requestPermission(userId) {
+  messaging.requestPermission()
+    .then(() => {
+      console.log('Notification permission granted.')
+      saveToken(userId)
+    })
+    .catch(error => {
+      console.error('Unable to get permission to notify.', error)
+    })
+}
