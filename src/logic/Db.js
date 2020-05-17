3d1cc04e626286f7457455aa6754424e8a962239
Filip <filip.kudla@snow.dog>
2020-05-17 17:45:30 +0200

    Add firebase functions only when browser supports it

--- src/logic/Db.js
+++ src/logic/Db.js
@@ -3,8 +3,9 @@ import 'firebase/database'
 import 'firebase/auth'
 import 'firebase/analytics'
 import 'firebase/messaging'
+import 'firebase/functions'
 
-const app = firebase.initializeApp({
+export const app = firebase.initializeApp({
   apiKey: 'AIzaSyA6ClIYD2m8sTIBP2ClityYPXdiw7mBCyg',
   authDomain: 'best-before-app-afb09.firebaseapp.com',
   databaseURL: 'https://best-before-app-afb09.firebaseio.com',
@@ -19,5 +20,7 @@ app.analytics()
 
 export const db = app.database()
 export const auth = app.auth()
-export const messaging = app.messaging()
+export const functions = app.functions()
+export const isMessagingAvailable = firebase.messaging.isSupported()
+export const messaging = isMessagingAvailable ? app.messaging() : false
 export const currentTime = firebase.database.ServerValue.TIMESTAMP
