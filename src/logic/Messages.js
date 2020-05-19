271e5d1c83a331ffa7958feef2319f6a15261d25
Filip <filip.kudla@snow.dog>
2020-05-20 00:10:48 +0200

    Add messages to notifyMessage variable in messages logic with future TODO

--- src/logic/Messages.js
+++ src/logic/Messages.js
@@ -1,6 +1,8 @@
 import { db, messaging } from './Db.js'
 import store from '../store/index.js'
 
+export let notifyMessage = ''
+
 // Saves the token to the database if available. If not request permissions
 export function saveToken(userId) {
   messaging.getToken()
@@ -12,41 +14,41 @@ export function saveToken(userId) {
       }
     })
     .catch(err => {
-      // TODO: Show this info in notify
-      let notifyMessage = 'An unidentified error has occurred'
-      console.error('Unable to get messaging token.', err)
+      notifyMessage = `An unidentified error has occurred - ${err.message}`
 
       if (err.code === 'messaging/permission-default') {
         notifyMessage = 'You have not enabled notifications on this browser. To enable notifications reload the page and allow notifications using the permission dialog.'
       } else if (err.code === 'messaging/notifications-blocked') {
         notifyMessage = 'You have blocked notifications on this browser. To enable notifications follow these instructions: <a href="https://support.google.com/chrome/answer/114662?visit_id=1-636150657126357237-2267048771&rd=1&co=GENIE.Platform%3DAndroid&oco=1">Android Chrome Instructions</a><a href="https://support.google.com/chrome/answer/6148059">Desktop Chrome Instructions</a>'
       }
-
-      console.log(notifyMessage)
     })
 }
 
 export function tokenRefresh() {
-  // TODO: Do some fallback for this usesr with info or something
+  // TODO: Refactor this function
   if (!messaging || !store.state.user) {
+    if (!messaging) {
+      notifyMessage = 'Your browser doesn\'t support Push API. We can\t deliver some features there'
+    }
     return false
   }
 
   messaging.onTokenRefresh(() => {
     messaging.getToken().then((refreshedToken) => {
       console.log('Token refreshed.')
-      // Send Instance ID token to app server
       saveToken(refreshedToken)
-    }).catch((err) => {
-      console.log('Unable to retrieve refreshed token ', err)
+    }).catch(err => {
+      notifyMessage = `Problem during refreshing token for user - ${err.message}`
     })
   })
 }
 
 // Requests permission to send notifications on this browser
 export function requestPermission() {
-  // TODO: Do some fallback for this usesr with info or something
   if (!messaging || !store.state.user) {
+    if (!messaging) {
+      notifyMessage = 'Your browser doesn\'t support Push API. We can\t deliver some features there'
+    }
     return false
   }
 
@@ -56,13 +58,11 @@ export function requestPermission() {
       saveToken(store.getters.getUserUid)
     })
     .catch(error => {
-      console.error('Unable to get permission to notify.', error)
+      notifyMessage = `Problem with getting permissions - ${error.message}`
     })
 }
 
 export function onMessage(payload) {
-  console.log('Notifications received.', payload);
-
   if (payload.notification) {
     // If notifications are supported on this browser we display one.
     if (window.Notification instanceof Function) {
